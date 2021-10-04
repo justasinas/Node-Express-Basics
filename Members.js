305a741b069fe7ec0c5d3f2239935eb06b9bc832
Justas <justasinas@gmail.com>
2021-10-04 13:19:56 +0300

    node

---
+++ Members.js
@@ -0,0 +1,27 @@
+const members = [
+    {
+        id: 1,
+        name: 'Justas Pra',
+        email: 'email@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Tomas Dra',
+        email: 'email@email.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Justinas Pra',
+        email: 'beleka@email.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Petras Pe',
+        email: 'Petris@email.com',
+        status: 'active'
+    }
+];
+module.exports = members;
